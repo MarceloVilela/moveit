9463833928f2dbad72656c5e8a1f79e2fc808a6f
Marcelo Vilela da Silva <marcelo.vilela.s@gmail.com>
2021-02-28 13:04:00 -0300

    feat: notifications, complete challenge

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,13 +1,18 @@
+import { useContext } from 'react'
+
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/marcelovilela.png" alt="Marcelo Vilela" />
 
       <div>
         <strong>Marcelo Vilela</strong>
-        <p>Level 1</p>
+        <p>Level {level}</p>
       </div>
     </div>
   )
